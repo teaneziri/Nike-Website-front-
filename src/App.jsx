3877e611bd487d127d4141e97c073e05d5abdb4c
teaneziri <neziritea@gmail.com>
2024-12-12 15:51:17 +0100

    structure of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App=()=>(
+<main className="relative">
+  Nav
+
+  <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+  <section className="padding">Popular Products</section>
+  <section className="padding">Super Quality</section>
+  <section className="padding-x py-10">Services</section>
+  <section className="padding">Special Offers</section>
+  <section className="bg-pale-blue padding">Customer Reviews</section>
+  <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+  <section className="bg-black padding-x padding-t pb-8">Footer</section>
+
+</main>
+
+)
+
+export default App;
