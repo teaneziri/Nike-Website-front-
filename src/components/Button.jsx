373fb50dd94b05c1967bfe28ede574ed7e8e6b38
teaneziri <neziritea@gmail.com>
2024-12-13 15:41:23 +0100

    Super Quality section finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,8 @@ const Button = ({label,iconURL}) => {
   return (
     <div className="flex justify-center  items-center px-7 gap-2 py-3 border border-coral-red text-lg leading-none bg-coral-red rounded-full text-white ">
         {label}
-        <img src={iconURL} alt="" className="ml-2 rounded-ful w-6 h-6"/>
+        {iconURL &&
+        <img src={iconURL} alt="" className="ml-2 rounded-ful w-6 h-6"/>}
     </div>
   )
 }
