1ce3085c7f509ae14f3edc69a4eca48e46d72051
teaneziri <neziritea@gmail.com>
2024-12-13 14:34:09 +0100

    Hero section finished

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
-import { statistics } from "../constants"
+import ShoeCard from "../components/ShoeCard"
+import { shoes, statistics } from "../constants"
 
 const Hero=()=>{
+
+  const [bigShoeImg,setBigShoeImg]=useState(bigShoe1)
   return(
 <section
 id="home"
-className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen max-container"
+className="w-full  p-2 flex xl:flex-row flex-col justify-center min-h-screen max-container"
 >
   <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
   <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
@@ -17,7 +22,7 @@ className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-
   <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort and innovative for your active life.</p>
   <Button label="Shop now" iconURL={arrowRight}/>
 
-  <div className="border-2 border-red-500 flex justify-start items-start flex-wrap w-full gap-16 mt-20">
+  <div className=" flex justify-start items-start flex-wrap w-full gap-16 mt-20">
    {statistics.map((stat)=>(
    <div key={stat.label}>
      <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
@@ -28,7 +33,25 @@ className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-
   </div>
   </div>
 
+  <div className=" relative flex-1 flex justify-center align-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+    <img src={bigShoeImg} alt="show collection" width={610} height={500} className="object-contain relative z-10"/>
+
+<div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+{shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+</div>
   
+  </div>
+
+ 
 
 </section>
   )
